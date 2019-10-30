ee61a942ffe7b9d40e3891549d56b16b790e2d31
Truc0810 <55777063+Truc0810@users.noreply.github.com>
2019-10-30 10:10:37 +0700

    layoyt_hotel
    edit layoyt_hotel

---
+++ Hotel_version3/Hotel/Migrations/20191027114707_CreateKeyPhieuDatPhongBill.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.Migrations
+{
+    public partial class CreateKeyPhieuDatPhongBill : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemAccount",
+                columns: table => new
+                {
+                    SystemAccountId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SystemAccountName = table.Column<string>(maxLength: 20, nullable: true),
+                    SystemAccountPassWords = table.Column<string>(maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemAccount", x => x.SystemAccountId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_PhieuDatPhongId",
+                table: "Bills",
+                column: "PhieuDatPhongId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bills_PhieuDatPhongs_PhieuDatPhongId",
+                table: "Bills",
+                column: "PhieuDatPhongId",
+                principalTable: "PhieuDatPhongs",
+                principalColumn: "PhieuDatPhongId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bills_PhieuDatPhongs_PhieuDatPhongId",
+                table: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "SystemAccount");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bills_PhieuDatPhongId",
+                table: "Bills");
+        }
+    }
+}
---
+++ Hotel_version3/Hotel/Models/Phong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Models
+{
+    public class Phong
+    {
+        [Key]
+        public int PhongId { get; set; }
+
+        public string TenPhong { get; set; }
+
+        public int SoNguoiOToiDa { get; set; }
+
+        public string MoTaPhong { get; set; }
+
+        public decimal CachTinhTien { get; set; }
+
+        public decimal PhuThu { get; set; }
+
+        //0: Phong can clean, 1: phong San sang, 2: Phong co nguoi, 3: PhongDaDatCoc
+        public int TinhTrangPhong { get; set; }
+
+        public int LoaiPhongId { get; set; }
+        public LoaiPhong LoaiPhongs { get; set; }
+
+
+        
+    }
+}
