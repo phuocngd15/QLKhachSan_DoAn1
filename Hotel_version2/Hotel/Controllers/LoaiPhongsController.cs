ee0c908980ca52d0a66c96ae432add5bd6bcd61a
Truc0810 <55777063+Truc0810@users.noreply.github.com>
2019-10-30 08:30:38 +0700

    HoanChinhCacLopVaMoiQuanHe
    sua va them cac moi quan he

---
+++ Hotel_version2/Hotel/Controllers/LoaiPhongsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotel.Data;
+using Hotel.Models;
+
+namespace Hotel.Controllers
+{
+    public class LoaiPhongsController : Controller
+    {
+        private readonly HotelDataContext _context;
+
+        public LoaiPhongsController(HotelDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LoaiPhongs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LoaiPhongs.ToListAsync());
+        }
+
+        // GET: LoaiPhongs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiPhong = await _context.LoaiPhongs
+                .FirstOrDefaultAsync(m => m.LoaiPhongId == id);
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiPhong);
+        }
+
+        // GET: LoaiPhongs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoaiPhongs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LoaiPhongId,TenLoaiPhong,MoTaLoaiPhong")] LoaiPhong loaiPhong)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(loaiPhong);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiPhong);
+        }
+
+        // GET: LoaiPhongs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiPhong = await _context.LoaiPhongs.FindAsync(id);
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+            return View(loaiPhong);
+        }
+
+        // POST: LoaiPhongs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LoaiPhongId,TenLoaiPhong,MoTaLoaiPhong")] LoaiPhong loaiPhong)
+        {
+            if (id != loaiPhong.LoaiPhongId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(loaiPhong);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoaiPhongExists(loaiPhong.LoaiPhongId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(loaiPhong);
+        }
+
+        // GET: LoaiPhongs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loaiPhong = await _context.LoaiPhongs
+                .FirstOrDefaultAsync(m => m.LoaiPhongId == id);
+            if (loaiPhong == null)
+            {
+                return NotFound();
+            }
+
+            return View(loaiPhong);
+        }
+
+        // POST: LoaiPhongs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var loaiPhong = await _context.LoaiPhongs.FindAsync(id);
+            _context.LoaiPhongs.Remove(loaiPhong);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoaiPhongExists(int id)
+        {
+            return _context.LoaiPhongs.Any(e => e.LoaiPhongId == id);
+        }
+    }
+}
---
+++ Hotel_version2/Hotel/Models/SystemAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel.Models
+{
+    public class SystemAccount
+    {
+        [Key]
+        public int SystemAccountId { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(20)]
+        public String SystemAccountName { get; set; }
+
+        [StringLength(20)]
+        public String SystemAccountPassWords { get; set; }
+    }
+}
