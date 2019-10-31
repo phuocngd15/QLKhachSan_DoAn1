1cb6195081eeb88dc182d34db1e11ed1416de5c4
Truc0810 <55777063+Truc0810@users.noreply.github.com>
2019-10-31 21:12:27 +0700

    ChangeClassChiTietHoaDon
    Thay doi thuoc tinh trong lop ChiTietHoaDon

---
+++ Hotel_version4/Hotel/Migrations/20191027062343_CreateKeyAccountUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel.Migrations
+{
+    public partial class CreateKeyAccountUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AccountId",
+                table: "Users",
+                column: "AccountId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "AccountId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Accounts_AccountId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AccountId",
+                table: "Users");
+        }
+    }
+}
