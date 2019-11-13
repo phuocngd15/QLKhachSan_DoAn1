441303a728e1defbecda4463d156f25ac6a2bb0e
Truc0810 <55777063+Truc0810@users.noreply.github.com>
2019-11-13 10:16:52 +0700

    Core3.0
    Core3.0

---
+++ Test/Test/Models/LoaiPhong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class LoaiPhong
+    {
+        public LoaiPhong()
+        {
+            Phongs = new HashSet<Phong>();
+        }
+
+        [Key]
+        public int LoaiPhongId { get; set; }
+
+        [Display(Name = "Tên Loại Phòng")]
+        public string TenLoaiPhong { get; set; }
+
+        [Display(Name = "Mô Tả Loại Phòng")]
+        public string MoTaLoaiPhong { get; set; }
+
+        public virtual ICollection<Phong> Phongs { get; set; }
+    }
+}
